79ae5caf3171f2e66b1f65c8984df53aea6e387c
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-27 13:08:53 +0900

    Update: fetch from cache on hasura-sub page

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -6,7 +6,9 @@ import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/layout'
 
 const FetchMain: VFC = () => {
-  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'network-only',
+  })
 
   if (error) {
     return (
