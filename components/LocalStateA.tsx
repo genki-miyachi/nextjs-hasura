55e5ce194a03d0c102722ac6b244643cc9435571
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-27 12:07:23 +0900

    Update: css mistake

--- components/LocalStateA.tsx
+++ components/LocalStateA.tsx
@@ -14,22 +14,21 @@ export const LocalStateA: VFC = () => {
 
   return (
     <>
-      <p className="mb-3 font-bold">
-        {todos.map((task, index) => {
-          return (
-            <p key={index} className="mb-3 font-bold">
-              {task.title}
-            </p>
-          )
-        })}
-      </p>
+      <p className="mb-3 font-bold">makeVar</p>
+      {todos?.map((task, index) => {
+        return (
+          <p className="mb-3 y-1" key={index}>
+            {task.title}
+          </p>
+        )
+      })}
       <form
-        action="flex flex-col justify-center item-center"
+        className="flex flex-col justify-center items-center"
         onSubmit={handleSubmit}
       >
         <input
-          className="mb-3 px-3 py-3 border border-gray-300"
-          placeholder="New task?"
+          className="mb-3 px-3 py-2 border border-gray-300"
+          placeholder="New task ?"
           value={input}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setInput(e.target.value)
@@ -37,7 +36,7 @@ export const LocalStateA: VFC = () => {
         />
         <button
           disabled={!input}
-          className="disabled:opacity-40 mb-3 py-1 px-3 text-white bg-indigo-600 hover: bg-indigo-700 rounded-2xl focus:outline-none"
+          className="disabled:opacity-40 mb-3 py-1 px-3 text-white bg-indigo-600 hover:bg-indigo-700 rounded-2xl focus:outline-none"
           type="submit"
         >
           Add new state
