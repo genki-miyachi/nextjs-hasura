1a67e088897cc52ef936868496a2981a3a459fcd
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-27 12:57:00 +0900

    Add: Create apollo client and fetch from Hasura

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,37 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+import { Layout } from '../components/layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user, index) => {
+        return (
+          <p className="my-1" key={index}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default FetchMain
